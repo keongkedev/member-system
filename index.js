a462949f0e74b3eb5f322b3bd3747ebfe2793943
keong ke <kjh.keong@gmail.com>
2024-08-31 12:22:41 +0800

    建立會員註冊

---
+++ index.js
@@ -0,0 +1,75 @@
+// 建立資料庫連線
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://root:root123@mycluster.flhjk.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster";
+let db = null;
+
+async function main() {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    db = client.db("menber-system");
+    console.log("資料庫連線成功");
+  } catch (err) {
+    console.log("連線失敗", err);
+  }
+}
+
+// 建立網站伺服器基礎設定
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.use(
+  session({
+    secret: "anything",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// 處理路由
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
+app.get("/member", function (req, res) {
+  res.render("member.ejs");
+});
+// 連線到 /error?msg=錯誤訊息
+app.get("/error", function (req, res) {
+  const msg = req.query.msg;
+  res.render("error.ejs", { msg: msg });
+});
+// 註冊會員
+app.post("/signup", async function (req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  // 檢查資料庫的資料
+  const collection = db.collection("member");
+  let result = await collection.findOne({
+    email: email,
+  });
+  if (result !== null) {
+    res.redirect("/error?msg=註冊失敗，信箱重複");
+    return;
+  }
+  // 將新的資料放到資料庫
+  result = await collection.insertOne({
+    name: name,
+    email: email,
+    password: password,
+  });
+  // 新增成功，導回首頁
+  res.redirect("/");
+});
+
+// 啟動伺服器
+main().then(() => {
+  app.listen(3000, function () {
+    console.log("Sever Started");
+  });
+});
